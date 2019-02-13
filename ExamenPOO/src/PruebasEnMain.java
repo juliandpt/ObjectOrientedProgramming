f12db2c83c5c3faefdfd8d77c40c66489d10420e
julidpt <38203898+julidpt@users.noreply.github.com>
2019-02-13 23:47:30 +0100

    Avanzado en ExamenPOO

--- ExamenPOO/src/PruebasEnMain.java
+++ ExamenPOO/src/PruebasEnMain.java
@@ -3,7 +3,13 @@ public class PruebasEnMain {
 
 	public static void main(String[] args) {
 		
+		System.out.println(Taller.comprobarLicencia(123473));
 		Taller taller = new Taller("Norauto", "c/ VALLESECO, 13", 1, "Juan Carlos", "57437596L");
+		Vehiculo vehiculo1 = new Vehiculo("Coupe", "BMW", "321456");
+		Vehiculo vehiculo2 = new Vehiculo("Coupe", "BMW", "321456");
+		Taller.anadirVehiculo(vehiculo1);
+		Taller.anadirVehiculo(vehiculo1);
+		Taller.eliminarVehiculo(vehiculo2);
 		
 	}
 
--- ExamenPOO/src/Taller.java
+++ ExamenPOO/src/Taller.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public abstract class Taller {
+public class Taller {
 
 	private String nombreTaller;
 	private String direccion;
@@ -30,7 +30,7 @@ public abstract class Taller {
 	
 	public String datosAdministrador() {
 		administrador = new Administrador("Jose", "32457684J");
-		return administrador.getNombre()+ ": " + administrador.getDNI();
+		return administrador.getNombre()+ " : " + administrador.getDNI();
 	}
 
 	public void aumentarAnosServicio() {
@@ -38,18 +38,34 @@ public abstract class Taller {
 	}
 	
 	public boolean anadirVehiculo (Vehiculo vehiculo) {
-		return vehiculos.add(vehiculo);		
+		if (vehiculos.contains(vehiculo)) {
+			return false;
+		}
+		
+		return vehiculos.add(vehiculo);	
 	}
 	
-	public boolean eliminarVehiculo (String matricula) {
-		return 
+	public boolean eliminarVehiculo (Vehiculo vehiculo) {
+		if (vehiculos.contains(vehiculo)) {
+			return true;
+		}
+		
+		return false;
 	}
 	
 	private boolean existeVehiculo (String matricula) {
+		if (vehiculos.contains(matricula)) {
+			return true;
+		}
 		
+		return false;
 	}
 	
 	public static boolean comprobarLicencia (int numLicencia) {
+		if (numLicencia % 10 == 0) {
+			return true;
+		}
 		
+		return false;
 	}
 }
