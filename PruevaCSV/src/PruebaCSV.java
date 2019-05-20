a9baf3a2e51b0ff215ead0d83279e91030134045
julidpt <38203898+julidpt@users.noreply.github.com>
2019-05-20 17:03:32 +0200

    avanzado con la clase PruebasCSV

--- PruevaCSV/src/PruebaCSV.java
+++ PruevaCSV/src/PruebaCSV.java
@@ -1,15 +1,38 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class PruebaCSV {
 	public static void main(String[] args) throws FileNotFoundException{
-		//leer datos del archivo
-		Scanner scanner = new Scanner(new File("C:\\Users\\julid\\Desktop\\PruebaCSV.csv"));
+		//leer datos del archivo, 1:"C:\\Users\\Juliandpt\\Desktop\\PruebasCSV.csv", 2:"C:\\Users\\julid\\Desktop\\PruebaCSV.csv"
+		Scanner scanner = new Scanner(new File("C:\\Users\\Juliandpt\\Desktop\\PruebasCSV.csv"));
 		scanner.useDelimiter(",");
         while(scanner.hasNext()){
             System.out.print(scanner.next()+"|");
         }
         scanner.close();
+        
+        System.out.println("--------------------------");
+        //meter datos
+        try (PrintWriter writer = new PrintWriter(new File("C:\\Users\\Juliandpt\\Desktop\\PruebasCSV.csv"))) {
+
+            StringBuilder sb = new StringBuilder();
+            sb.append("id,");
+            sb.append(',');
+            sb.append("Name");
+            sb.append('\n');
+
+            sb.append("1");
+            sb.append(',');
+            sb.append("Prashant Ghimire");
+            sb.append('\n');
+
+
+            System.out.println("done!");
+
+          } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+          }
 	}
 }
